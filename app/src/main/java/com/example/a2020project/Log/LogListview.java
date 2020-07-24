7c636ba8c171df2843408ab5bbfef857288d84b4
Ssong <songhui575@gmail.com>
2020-07-25 03:20:05 +0900

    log탭 추가

---
+++ app/src/main/java/com/example/a2020project/Log/LogListview.java
@@ -0,0 +1,20 @@
+package com.example.a2020project.Log;
+
+public class LogListview {
+    private String titleStr;
+    private String descStr;
+
+    public void setDescStr(String descStr) {
+        this.descStr = descStr;
+    }
+    public void setTitleStr(String titleStr) {
+        this.titleStr = titleStr;
+    }
+
+    public String getDescStr() {
+        return descStr;
+    }
+    public String getTitleStr() {
+        return titleStr;
+    }
+}
