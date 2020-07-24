ce810b7d0abef38f6bba214fe5ddf8a76dbf9ef5
Ssong <songhui575@gmail.com>
2020-07-25 03:19:17 +0900

    log탭 추가, 수정

--- app/src/main/java/com/example/a2020project/Log/Log.java
+++ app/src/main/java/com/example/a2020project/Log/Log.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.a2020project.R;
 
@@ -23,8 +25,80 @@ public class Log extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_log, null) ;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log, container, false);
+        ListView listview1;
+        ListView listview2;
+        ListView listview3;
+        LogListviewAdapter adapter1;
+        LogListviewAdapter adapter2;
+        LogListviewAdapter adapter3;
+
+        // Adapter 생성
+        adapter1 = new LogListviewAdapter();
+        adapter2 = new LogListviewAdapter();
+        adapter3 = new LogListviewAdapter();
+
+        // 리스트뷰 참조 및 Adapter 닫기
+        listview1 = view.findViewById(R.id.listView1);
+        listview2 = view.findViewById(R.id.listView2);
+        listview3 = view.findViewById(R.id.listView3);
+
+
+        listview1.setAdapter(adapter1);
+        listview2.setAdapter(adapter2);
+        listview3.setAdapter(adapter3);
+
+        // 첫 번째 아이템 추가
+        adapter1.addItem("Box", "Account Box black 36dp");
+        adapter2.addItem("Box", "Account Box black 36dp");
+        adapter3.addItem("Box", "Account Box black 36dp");
+        // 두 번째 아이템 추가
+        adapter1.addItem("Circle", "Account Circle black 36dp");
+        adapter2.addItem("Circle", "Account Circle black 36dp");
+        adapter3.addItem("Circle", "Account Circle black 36dp");
+        // 세 번째 아이템 추가
+        adapter1.addItem("Ind", "Account Ind black 36dp");
+        adapter2.addItem("Ind", "Account Ind black 36dp");
+        adapter3.addItem("Ind", "Account Ind black 36dp");
+
+
+        listview1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // get item
+                LogListview item = (LogListview) parent.getItemAtPosition(position);
+
+                String titleStr = item.getTitleStr();
+                String descStr = item.getDescStr();
+            }
+        });
+
+
+        listview2.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // get item
+                LogListview item = (LogListview) parent.getItemAtPosition(position);
+
+                String titleStr = item.getTitleStr();
+                String descStr = item.getDescStr();
+            }
+        });
+
+       /* listview3.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // get item
+                ListViewItem item = (ListViewItem) parent.getItemAtPosition(position);
+
+                String titleStr = item.getTitleStr();
+                String descStr = item.getDescStr();
+            }
+        });*/
+        return view;
+        //return inflater.inflate(R.layout.fragment_log, container, false);
     }
 
 }
