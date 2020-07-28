f98ee2fe713605dd42bc5edcc2c0ff852f932874
Ssong <songhui575@gmail.com>
2020-07-29 00:04:40 +0900

    로그인 기능 추가(무조건 성공)

---
+++ app/src/main/java/com/example/a2020project/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.a2020project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText userId;
+    EditText userPw;
+    Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        userId = findViewById(R.id.loginId);
+        userPw = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(getApplicationContext(),"로그인 기능 임시 구현 무조건 성공", Toast.LENGTH_SHORT);
+
+                //intent.putExtra("userID", userId.getText().toString());
+
+            }
+        });
+
+
+    }
+
+}
