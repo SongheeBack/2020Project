0e2bb6bb27f52c049b41b78f18e8430e07aee041
Ssong <songhui575@gmail.com>
2020-08-28 00:43:26 +0900

    DB 연결 코드 분리

--- app/src/main/java/com/example/a2020project/monitor/Monitor.java
+++ app/src/main/java/com/example/a2020project/monitor/Monitor.java
@@ -1,16 +1,40 @@
 package com.example.a2020project.monitor;
 
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.a2020project.DBConnect;
+import com.example.a2020project.MainActivity;
 import com.example.a2020project.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -18,6 +42,16 @@ import com.example.a2020project.R;
  */
 public class Monitor extends Fragment {
 
+    /*private static String TAG_Query = "";
+    private static final String TAG_JSON="webnautes";
+    private static final String TAG_ID = "device_id";
+    private static final String TAG_NAME = "device_name";
+    private static final String TAG_DATA = "data";*/
+
+    ArrayList<HashMap<String, String>> mArrayList;
+    String mJsonString;
+    ListView listview;
+    MonitorListviewAdapter mAdapter;
 
     public Monitor() {
         // Required empty public constructor
@@ -28,25 +62,63 @@ public class Monitor extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_monitor, null) ;
-        ListView listview ;
-        MonitorListviewAdapter mAdapter;
+        //View view = inflater.inflate(R.layout.fragment_monitor, null) ;
+
+        //return view;
+        return inflater.inflate(R.layout.fragment_monitor, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
 
         // Adapter 생성
-        mAdapter = new MonitorListviewAdapter() ;
+        mAdapter = new MonitorListviewAdapter();
 
         // 리스트뷰 참조 및 Adapter달기
         listview = view.findViewById(R.id.monitorListview);
-        listview.setAdapter(mAdapter);
 
-        // 디비에서 데이터 가져와서
-        // mAdapter.addItem 이용해 데이터 추가
-        // 동적
-        mAdapter.addItem("디바이스1", "20");
-        mAdapter.addItem("디바이스2", "40");
+        try{
 
-        return view;
-        //return inflater.inflate(R.layout.fragment_monitor, container, false);
+            DBConnect DBcon = new DBConnect();
+            DBConnect.GetData db = DBcon.new GetData();
+            mJsonString = db.execute("select device_ID, device_name from device", "2").get();
+            showResult();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void showResult(){
+        try {
+
+            JSONArray jsonArray = new JSONArray(mJsonString);
+
+            for(int i=0;i<jsonArray.length();i++){
+
+                JSONArray item = jsonArray.getJSONArray(i);
+
+                String id = item.getString(0);
+                String data = item.getString(1);
+
+                mAdapter.addItem(data, id);
+
+                /*HashMap<String,String> hashMap = new HashMap<>();
+
+                hashMap.put(TAG_ID, id);
+                hashMap.put(TAG_NAME, name);
+
+                mArrayList.add(hashMap);*/
+            }
+
+            listview.setAdapter(mAdapter);
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
     }
 
 }
